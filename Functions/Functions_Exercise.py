8b129dd3af112c5e21fd43452ddc30ea2787b3ab
YavorMarkov <yavormaxpower@gmail.com>
2023-07-13 17:54:41 +0300

    add example 2

--- Functions/Functions_Exercise.py
+++ Functions/Functions_Exercise.py
@@ -1,3 +1,11 @@
+#  example 1:
 def greet(name):
     print(f"Hello {name}" )
-greet("Yavor") # Output: Hello, Alice! 
+greet("Yavor") # Output: Hello, Alice!
+
+# example 2:
+#Return Value
+def add_numbers(num1, num2):
+    return num1 + num2 
+sum = add_numbers(3, 4)
+print(sum) #Output 7
