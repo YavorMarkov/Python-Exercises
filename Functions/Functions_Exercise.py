111a7f1bb422e1b9502e9336ae37cab07ef68501
YavorMarkov <yavormaxpower@gmail.com>
2023-07-13 18:11:38 +0300

    add example 3 test

--- Functions/Functions_Exercise.py
+++ Functions/Functions_Exercise.py
@@ -9,3 +9,10 @@ def add_numbers(num1, num2):
     return num1 + num2 
 sum = add_numbers(3, 4)
 print(sum) #Output 7
+
+# example 3:
+def green(name='world'):
+    print(f"Hello, {name}!")
+
+greet() # Output: Hello world!
+greet("Yavor") #Output, hello Yavor"n  
