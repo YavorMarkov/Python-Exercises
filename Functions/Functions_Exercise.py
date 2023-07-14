908bbbbde0d4cfbed45d3578a04136814d55107d
YavorMarkov <yavormaxpower@gmail.com>
2023-07-14 14:47:14 +0300

    add Arbitrary Number of Arguments example

--- Functions/Functions_Exercise.py
+++ Functions/Functions_Exercise.py
@@ -11,8 +11,20 @@ sum = add_numbers(3, 4)
 print(sum) #Output 7
 
 # example 3:
+# Default Values
 def greet(name='world'):
     print(f"Hello, {name}!")
 
 greet() # Output: Hello world!
 greet("Yavor") #Output, hello Yavor"n  
+
+# Example 4 
+# Arbitrary Number of Arguments 
+
+def make_pizza(*toppings):
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
